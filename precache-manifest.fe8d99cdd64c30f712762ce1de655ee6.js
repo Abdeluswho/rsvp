3c82ba0ec88daf2a5b4e4fc9d1c1d1136af99ca9
A.Sofiane Guerfi <schguerfi@gmail.com>
2019-09-05 16:55:30 -0700

    Updates

---
+++ precache-manifest.fe8d99cdd64c30f712762ce1de655ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c7f74376635464b3b99c6621219861",
+    "url": "/rsvp/index.html"
+  },
+  {
+    "revision": "986228db6483d5005655",
+    "url": "/rsvp/static/css/main.9a940009.chunk.css"
+  },
+  {
+    "revision": "c814a92ffd944148d41c",
+    "url": "/rsvp/static/js/2.d5fb17a4.chunk.js"
+  },
+  {
+    "revision": "986228db6483d5005655",
+    "url": "/rsvp/static/js/main.d42c92c0.chunk.js"
+  },
+  {
+    "revision": "92be1949649659ab58a0",
+    "url": "/rsvp/static/js/runtime~main.67e21edb.js"
+  },
+  {
+    "revision": "be92ab00f0f555419adb491aa392b9ca",
+    "url": "/rsvp/static/media/header-bg.be92ab00.jpg"
+  }
+]);
